d2ecde6e7f56e2a132f0daa53611aa98803ae2a2
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-07-25 22:17:00 +1000

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 10f;
+    public float radius = 20f;
+
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if (Vector3.Distance(transform.position, target.position) < radius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    private CharacterController charC;
+
+    void Start()
+    {
+        charC = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        charC.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime);
+    }
+}
